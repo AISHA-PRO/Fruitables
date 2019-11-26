457206a557fcba41690f22354cb0070a37fca5cd
AimanAlmureish <aiman.almureish@gmail.com>
2019-11-26 12:51:41 +0300

    Updates

---
+++ precache-manifest.8e2aba44c86f11d06d5047d648fd76e4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7386f31c44823728b726306814e44681",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d358a35bf7228943dad0",
+    "url": "/static/css/main.2660170f.chunk.css"
+  },
+  {
+    "revision": "6d93135aa6ed564b289c",
+    "url": "/static/js/2.2fc4f330.chunk.js"
+  },
+  {
+    "revision": "d358a35bf7228943dad0",
+    "url": "/static/js/main.9ecbdbfa.chunk.js"
+  },
+  {
+    "revision": "a2f72eae3fdd47a76cf7",
+    "url": "/static/js/runtime-main.98a606d5.js"
+  },
+  {
+    "revision": "a8d48085d81ea53cd8656b7f9b25ea93",
+    "url": "/static/media/apples.a8d48085.jpg"
+  },
+  {
+    "revision": "b024d5d8177edbfc11ea966e3b54053d",
+    "url": "/static/media/strawberries.b024d5d8.jpg"
+  },
+  {
+    "revision": "6900bed11af19004c6daf6a9e5dbb8a0",
+    "url": "/static/media/tomatoes.6900bed1.jpg"
+  }
+]);
